1c7c86ab37826621da3f1c39a9ad1f63e2d452a7
mimipaskova <maria.paskova3@gmail.com>
2014-03-26 00:41:37 +0200

    connect with sqlite break

--- week4/manage_company.py
+++ week4/manage_company.py
@@ -1,27 +1,47 @@
 import sqlite3
 
+def create_connect():
+    conn = sqlite3.connect("create_bank")
+    c = conn.cursor()
+
+
+def list_employees(c):
+    my_list = "SELECT name, position FROM company;"
+    print(c.execute(my_list))
+
 def command_split(command):
     return command.split(" ")
 
 
 def main():
     while True:
+        create_connect()
         command = command_split(input("command>"))
 
         if command[0] == "list_employees":
+            list_employees(c)
             print(command)
         elif command[0] == "monthly_spending":
+            print(command)
+            break
 
         elif command[0] == "yearly_spending":
-
+            print(command)
+            break
         elif command[0] == "add_employee":
+            print(command)
+            break
 
         elif command[0] == "delete_employee <employee_id>":
+            print(command)
+            break
 
         elif command[0] == "update_employee <employee_id>":
+            print(command)
+            break
 
         elif command[0] == "help":
-        break
+            break
 
 
 if __name__ == '__main__':

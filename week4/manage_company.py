c1c4d1ace22446b6902f65059c69b04de62f33aa
mimipaskova <maria.paskova3@gmail.com>
2014-03-25 17:13:22 +0200

    manage company UI

--- week4/manage_company.py
+++ week4/manage_company.py
@@ -10,6 +10,17 @@ def main():
 
         if command[0] == "list_employees":
             print(command)
+        elif command[0] == "monthly_spending":
+
+        elif command[0] == "yearly_spending":
+
+        elif command[0] == "add_employee":
+
+        elif command[0] == "delete_employee <employee_id>":
+
+        elif command[0] == "update_employee <employee_id>":
+
+        elif command[0] == "help":
         break
 
 
